4c3eb3b13808d865640f41b2efb8fc25ab513a2a
Whitespots <65983771+whitespots@users.noreply.github.com>
2020-10-27 21:05:09 +0400

    Update check.py

--- check.py
+++ check.py
@@ -1,12 +1,27 @@
 import requests
 import os
 import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-payload = 'kek.kek'
-xss_payload = 'kek.kek"onload="alert();'
 
-url = 'https://{0}'.format(os.environ.get('DOMAIN'))
+ports = os.environ.get('PORTS')
+ports = ports.strip(' ').split(',')
+urls = ['https://{0}/'.format(os.environ.get('DOMAIN'))]
+try:
+    for port in ports:
+        urls.append('http://{0}:{1}/'.format(os.environ.get('DOMAIN'), port))
+except:
+    pass
 vuln_id = os.environ.get('VULN_ID')
+
+
+def resp(state=False, url='https://{0}/'.format(os.environ.get('DOMAIN'))):
+    if state:
+        return json.dumps({"vulnerable": "True", "vuln_id": vuln_id, "description": url})
+    else:
+        return json.dumps({"vulnerable": "False", "vuln_id": vuln_id, "description": url})
+
 
 path_list = [
     '/admin',
@@ -38,18 +53,19 @@ stop_words = [
 
 stop_words_path = stop_words
 
-for path in path_list:
-    stop_words_path.append(f'{url}{path}')
+for url in urls:
+    for path in path_list:
+        stop_words_path.append(f'{url}{path}')
 
 
-def prepare_requests():
+def prepare_requests(url):
     result = []
     for path in path_list:
         result.append(f'{url}{path}')
     return result
 
 
-def resp(state=False, description=url):
+def resp(state=False, description=urls[0]):
     if state:
         return json.dumps({"vulnerable": "True", "vuln_id": vuln_id, "description": description})
     else:
@@ -57,28 +73,26 @@ def resp(state=False, description=url):
 
 
 def check():
-    if not url:
-        return resp(False)
-
-    candidates = prepare_requests()
-    try:
-        for candidate in candidates:
-            candidate_score = 0
-            response = requests.get(candidate, timeout=4)
-            for admin_pattern in admin_patterns:
-                if response.text.find(admin_pattern) > -1:
-                    candidate_score += 15
-            for stop_word in stop_words:
-                if response.text.find(stop_word) > -1:
-                    candidate_score -= 15
-            for stop_word in stop_words_path:
-                if response.text.find(stop_word) > -1:
-                    candidate_score -= 5
-            if candidate_score >= 5:
-                return resp(True, candidate)
-        return resp(False)
-    except Exception as ex:
-        pass
+    for url in urls:
+        candidates = prepare_requests(url)
+        try:
+            for candidate in candidates:
+                candidate_score = 0
+                response = requests.get(candidate, timeout=4, verify=False)
+                for admin_pattern in admin_patterns:
+                    if response.text.find(admin_pattern) > -1:
+                        candidate_score += 15
+                for stop_word in stop_words:
+                    if response.text.find(stop_word) > -1:
+                        candidate_score -= 15
+                for stop_word in stop_words_path:
+                    if response.text.find(stop_word) > -1:
+                        candidate_score -= 5
+                if candidate_score >= 5:
+                    return resp(True, candidate)
+            return resp(False)
+        except Exception as ex:
+            pass
     return resp(False)
 
 
